1defde8668ae058395d7eb1ab203abfa49d0cd3e
ashishahujaa450 <ahuja3594@gmail.com>
2019-12-16 14:46:04 +0530

    going to start income model refactoring

--- ts/View/AppView.ts
+++ ts/View/AppView.ts
@@ -16,32 +16,46 @@ export class AppView extends View<Budget> {
         <!-- title -->
         <h3 class="text-uppercase mb-4">budget app</h3>
         <div class="row">
-            <div class="col-md-4 my-3 budget-view" id="budgetView">
+            <div class="col-md-3 my-3 budget-view" id="budgetView">
                 <!-- budget feedback -->
                 <div class="budget-feedback alert alert-danger text-capitalize">
                     budget feedback
                 </div>
                 <!-- budget form -->
             </div>
-            <div class="col-md-8" id="dashboardView">
+            <div class="col-md-9" id="dashboardView">
                 <!-- app info -->
             </div>
         </div>
 
         <div class="row">
-            <div class="col-md-4 my-3" id="expenseAdderView">
+            <div class="col-md-3 my-3" id="expenseAdderView">
                 <!-- expense feedback -->
                 <div class="expense-feedback alert alert-danger text-capitalize">
                     expense feedback
                 </div>
                 <!-- expense form -->
             </div>
-            <div class="col-md-8 my-3">
+            <div class="col-md-9 my-3">
+
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="expense-list__info d-flex justify-content-between text-capitalize">
+                      <h5 class="list-item">expense title</h5>
+                      <h5 class="list-item">expense value</h5>
+                      <h5 class="list-item"></h5>
+                  </div>
+                </div>
+
+                <div class="col-md-6">
                 <div class="expense-list__info d-flex justify-content-between text-capitalize">
-                    <h5 class="list-item">expense title</h5>
-                    <h5 class="list-item">expense value</h5>
+                    <h5 class="list-item">Income title</h5>
+                    <h5 class="list-item">Income value</h5>
                     <h5 class="list-item"></h5>
                 </div>
+              </div>
+              </div>
+                
                 <!-- expense list -->
                 <div class="row">
 
@@ -62,7 +76,7 @@ export class AppView extends View<Budget> {
         </div>
 
         <div class="row">
-            <div class="col-md-4 my-3" id="incomeAdderView">
+            <div class="col-md-3 my-3" id="incomeAdderView">
                 <!-- expense feedback -->
                 <div class="expense-feedback alert alert-danger text-capitalize">
                     expense feedback
